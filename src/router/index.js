ae929321822280b07fbb16409c2491f95f97350a
nunomeira11 <40220240@esmad.ipp.pt>
2024-05-15 21:56:12 +0100

    criaçao de marcação de consultas

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LandingPageView from '../views/LandingPageView.vue'
 import LoginView from '../views/LoginView.vue'
 import RegisterView from '../views/RegisterView.vue'
+import MarcacaoView from '../views/MarcacaoView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: '/register',
       name: 'register',
       component: RegisterView
+    },
+    {
+      path: '/marcacao',
+      name: 'marcacao',
+      component: MarcacaoView
     },
   ]
 })
