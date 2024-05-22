f761531b9af85b359c556463701b39f201987beb
nunomeira11 <40220240@esmad.ipp.pt>
2024-05-22 12:05:00 +0100

    criaçao da pagina especialidades

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import LandingPageView from '../views/LandingPageView.vue'
 import LoginView from '../views/LoginView.vue'
 import RegisterView from '../views/RegisterView.vue'
 import DadosMarcacaoView from '../views/DadosMarcacaoView.vue'
+import EspecialidadesView from '../views/EspecialidadesView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -26,6 +27,11 @@ const router = createRouter({
       path: '/dadosmarcacao',
       name: 'dadosmarcacao',
       component: DadosMarcacaoView
+    },
+    {
+      path: '/especialidades',
+      name: 'especialidades',
+      component: EspecialidadesView
     },
   ]
 })
