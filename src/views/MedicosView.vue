4688c05ed3e1cd1b4aa7c2a716ecf35cd2c83d62
nunomeira11 <40220240@esmad.ipp.pt>
2024-05-22 14:31:25 +0100

    atualização

--- src/views/MedicosView.vue
+++ src/views/MedicosView.vue
@@ -2,28 +2,30 @@
     <div class="container">
         <!-- Barra lateral -->
         <div class="sidebar">
-          <!-- Conteúdo da barra lateral aqui -->
-          <ul class="sidebar-icons">
-            <img src="../img/myclinic.png" alt="Logo" class="sidebar-logo">
-            <router-link to="/home">
-                <img src="../img/noun-house-431615.png" alt="Inicio" class="sidebar-icon">
-            </router-link>
-            <router-link to="/dadosmarcacao">
-              <img src="../img/noun-plus-669265.png" alt="Marcações" class="sidebar-icon">
-            </router-link>
-            <img src="../img/noun-calender-6465838.png" alt="Agenda" class="sidebar-icon">
-            <router-link to="/especialidades">
-                <img src="../img/noun-doctor-79152.png" alt="Médicos" class="sidebar-icon">
-            </router-link>
-            <router-link to="/especialidades">
-              <img src="../img/noun-diagnosis-6672493.png" alt="Especialidades" class="sidebar-icon">
-            </router-link>
-            <img src="../img/noun-account-2049214.png" alt="Perfil" class="sidebar-icon">
-            <router-link to="/login">
-              <img src="../img/noun-logout-6733782.png" alt="Terminar Sessão" class="sidebar-icon">
-            </router-link>
-          </ul>
-        </div>
+            <!-- Conteúdo da barra lateral aqui -->
+            <ul class="sidebar-icons">
+              <img src="../img/myclinic.png" alt="Logo" class="sidebar-logo">
+              <router-link to="/home">
+                  <img src="../img/noun-house-431615.png" alt="Inicio" class="sidebar-icon">
+              </router-link>
+              <router-link to="/dadosmarcacao">
+                <img src="../img/noun-plus-669265.png" alt="Marcações" class="sidebar-icon">
+              </router-link>
+              <img src="../img/noun-calender-6465838.png" alt="Agenda" class="sidebar-icon">
+              <router-link to="/medicos">
+                  <img src="../img/noun-doctor-79152.png" alt="Médicos" class="sidebar-icon">
+              </router-link>
+              <router-link to="/especialidades">
+                <img src="../img/noun-diagnosis-6672493.png" alt="Especialidades" class="sidebar-icon">
+              </router-link>
+              <router-link to="/user">
+                  <img src="../img/noun-account-2049214.png" alt="Perfil" class="sidebar-icon">
+              </router-link>
+              <router-link to="/login">
+                <img src="../img/noun-logout-6733782.png" alt="Terminar Sessão" class="sidebar-icon">
+              </router-link>
+            </ul>
+          </div>
         <div class="content">
           <select v-model="selectedMedico" required>
               <option value="" disabled selected>Selecione o Médico</option>
