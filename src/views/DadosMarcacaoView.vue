5035c7c6a0034ddde3b6cf3e16d957e67be52e8e
nunomeira11 <40220240@esmad.ipp.pt>
2024-05-31 12:20:40 +0100

    atualização

--- src/views/DadosMarcacaoView.vue
+++ src/views/DadosMarcacaoView.vue
@@ -1,38 +1,39 @@
 <template>
+
   <div class="container">
-    <!-- Barra lateral -->
-    <div class="sidebar">
-      <!-- Conteúdo da barra lateral aqui -->
-      <ul class="sidebar-icons">
-        <img src="../img/myclinic.png" alt="Logo" class="sidebar-logo">
-        <router-link to="/home">
-            <img src="../img/noun-house-431615.png" alt="Inicio" class="sidebar-icon">
-        </router-link>
-        <router-link to="/dadosmarcacao">
-          <img src="../img/noun-plus-669265.png" alt="Marcações" class="sidebar-icon">
-        </router-link>
-        <img src="../img/noun-calender-6465838.png" alt="Agenda" class="sidebar-icon">
-        <router-link to="/medicos">
-            <img src="../img/noun-doctor-79152.png" alt="Médicos" class="sidebar-icon">
-        </router-link>
-        <router-link to="/especialidades">
-          <img src="../img/noun-diagnosis-6672493.png" alt="Especialidades" class="sidebar-icon">
-        </router-link>
-        <router-link to="/user">
-            <img src="../img/noun-account-2049214.png" alt="Perfil" class="sidebar-icon">
-        </router-link>
-        <router-link to="/login">
-          <img src="../img/noun-logout-6733782.png" alt="Terminar Sessão" class="sidebar-icon">
-        </router-link>
-      </ul>
-    </div>
+      <!-- Barra lateral -->
+      <div class="sidebar">
+        <!-- Conteúdo da barra lateral aqui -->
+        <ul class="sidebar-icons">
+          <img src="../img/myclinic.png" alt="Logo" class="sidebar-logo">
+          <router-link to="/home">
+              <img src="../img/noun-house-431615.png" alt="Inicio" class="sidebar-icon">
+          </router-link>
+          <router-link to="/dadosmarcacao">
+            <img src="../img/noun-plus-669265.png" alt="Marcações" class="sidebar-icon">
+          </router-link>
+          <img src="../img/noun-calender-6465838.png" alt="Agenda" class="sidebar-icon">
+          <router-link to="/medicos">
+              <img src="../img/noun-doctor-79152.png" alt="Médicos" class="sidebar-icon">
+          </router-link>
+          <router-link to="/especialidades">
+            <img src="../img/noun-diagnosis-6672493.png" alt="Especialidades" class="sidebar-icon">
+          </router-link>
+          <router-link to="/user">
+              <img src="../img/noun-account-2049214.png" alt="Perfil" class="sidebar-icon">
+          </router-link>
+          <router-link to="/login">
+            <img src="../img/noun-logout-6733782.png" alt="Terminar Sessão" class="sidebar-icon">
+          </router-link>
+        </ul>
+      </div>
 
     <div class="myclinic">
       <div class="progress-bar">
         <!-- Barra de progresso com etapas -->
         <span class="active">1. Dados da Marcação</span>
-        <span>3. Confirmação</span>
-    </div>
+        <span>2. Confirmação</span>
+      </div>
       <form class="appointment-form" @submit.prevent="submitForm">
 
         <div class="input-group">
@@ -101,7 +102,6 @@ export default {
 .container {
     display: flex;
     flex-direction: row;
-    overflow: hidden;
     }
   
   .sidebar {
@@ -129,24 +129,26 @@ export default {
     font-family: 'Arial', sans-serif;
     color: #000000;
     padding: 20px;
+    margin-left: auto;
+    margin-right: auto;
 
   }
   
     /* Barra de progresso e etapas */
     .progress-bar {
-      display: flex;
-      justify-content: center;
-      margin-bottom: 20px;
+      margin-bottom: 20px;  
     }
     
     .progress-bar span {
       background: #f0f0f0;
       padding: 5px 10px;
       border-radius: 5px;
-      margin-right: 250px; /* Adiciona margem à direita de cada span */
+ 
+      margin-right: 25%;
     }
   
   .progress-bar span.active {
+    margin-left: 15%;
     background: #04D0AB;
     color: black;
   }
