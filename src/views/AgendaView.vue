3bc109e8a0cfff88bc0c5c8c9e266f06694cec28
nunomeira11 <40220240@esmad.ipp.pt>
2024-06-11 11:56:41 +0100

    atualização

--- src/views/AgendaView.vue
+++ src/views/AgendaView.vue
@@ -1,40 +1,85 @@
 <template>
-    <div class="container">
+  <div class="container">
       <!-- Barra lateral -->
       <Sidebar/>
-  
-      <div class="myclinic">
-  
+
+      <div class="content">
+          <div class="white-rectangle">
+            <h2>Consulta de Psicologia - 14/05/02023, 16:00h</h2>
+              <router-link to="desmarcacao">
+                  <button class="desmarcar-button">Desmarcar Consulta</button>
+              </router-link>
+          </div>
       </div>
-    </div>
-  </template>
-  
-  <script>
-  import Sidebar from "../components/sidebar.vue"; 
+  </div>
+</template>
 
-
+<script>
+import Sidebar from "../components/sidebar.vue"; 
   export default {
-    components: {
-    Sidebar, 
-  },
-    data() {
-    },
-  };
-  </script>
-  
-  <style scoped>
-  .container {
-    display: flex;
-    flex-direction: row;
+      components: {
+          Sidebar,
+      },
+      data() {
+          return {
+              selectedEspecialidade: ''
+          }
+      }
   }
-  
-  .myclinic {
-    font-family: 'Arial', sans-serif;
-    color: #000000;
-    padding: 20px;
-    margin-left: auto;
-    margin-right: auto;
-  }
+</script>
 
-  </style>
-  
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: row;
+}
+
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 20px;
+}
+
+select {
+  width: 300px;
+  height: 50px;
+  padding: 10px;
+  margin-top: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+.white-rectangle {
+  width: 1250px; 
+  height: 100px;
+  padding: 50px; 
+  background-color: white; 
+  margin-top: 20px; 
+  border-radius: 5px;
+  display: flex; /* Layout flexível */
+  justify-content: space-between; /* Espaçar elementos */
+  align-items: center; /* Alinhar verticalmente */
+  border: 1px solid black;
+}
+
+.white-rectangle h2 {
+  margin: 0;
+  font-size: 30px;
+}
+
+.white-rectangle p {
+  margin: 5px 0 0 0;
+  font-size: 16px;
+}
+
+.desmarcar-button {
+  background: red; 
+  color: white; 
+  border: none;
+  border-radius: 25px; 
+  padding: 10px 20px; 
+  font-size: 16px; 
+  cursor: pointer; 
+}
+</style>
